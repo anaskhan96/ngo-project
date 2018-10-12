0315a1085934bc3cb582d219419472e2168e9b5a
Anas Khan <anas.khan96@outlook.com>
2018-10-12 12:19:55 +0530

    using mongo server on anask.xyz w/ auth

--- server/middleware/db.js
+++ server/middleware/db.js
@@ -1,8 +1,9 @@
 'use strict';
 
+require('dotenv').config();
 function initDB(callback) {
 	let mongoClient = require('mongodb').MongoClient;
-	mongoClient.connect('mongodb://localhost:27017', function(err, client) {
+	mongoClient.connect('mongodb://'+process.env.DB_USER+':'+process.env.DB_PASS+'@anask.xyz:27017', function(err, client) {
 		if (!err) {
 			console.log('Connected to mongo database at port 27017');
 		} else {
