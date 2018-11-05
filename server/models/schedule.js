bc293be7e720ce187fba7cbf4960f95f8c270106
Anas Khan <anas.khan96@outlook.com>
2018-11-05 12:20:45 +0530

    schedule model: first draft

---
+++ server/models/schedule.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const scheduleSchema = mongoose.Schema({
+	name: {
+		type: String
+	},
+	workDescription: {
+		type: String
+	},
+	class: {
+		type: String
+	},
+	days: {
+		type: String
+	},
+	subject: {
+		type: String
+	}
+}, {
+	timestamps: true
+});
+
+module.exports = mongoose.model('schedule', scheduleSchema, 'schedule');
